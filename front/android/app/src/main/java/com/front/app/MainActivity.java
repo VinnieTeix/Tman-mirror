ea2913ab643ba2c7153897543444f74e340a5ed0
Danny DOMINGUES <danny.domingues@outlook.fr>
2024-10-27 16:32:17 +0100

    Feat: add capacitor

---
+++ front/android/app/src/main/java/com/front/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.front.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.front.app',
+  appName: 'front',
+  webDir: 'dist'
+};
+
+export default config;
